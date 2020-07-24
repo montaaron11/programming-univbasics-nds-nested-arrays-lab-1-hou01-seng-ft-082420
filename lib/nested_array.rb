022f224c7a739740054b66cf5fdd42089892c383
Aaron Montgomery <montaaron11@gmail.com>
2020-07-24 06:45:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -24,6 +24,22 @@ def assembled_matrix
 end
 
 def sorted_matrix
+  storage_room2 = [
+    [
+      "Eggplant", 
+      "Grapefruit", 
+      "Oranges", 
+      "Pineapple", 
+      "Watermelon"
+      ],
+    [
+      "Asparagus", 
+      "Avocadoes", 
+      "Grapes", 
+      "Potatoes", 
+      "Strawberries"
+      ]
+    ]
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
